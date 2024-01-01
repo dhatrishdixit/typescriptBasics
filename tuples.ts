a6dc311794416fe32210b033a4c7c4f517757ee1
dhatrishdixit <dhatrish29@gmail.com>
2024-01-01 12:25:45 +0530

    tupels

---
+++ tuples.ts
@@ -0,0 +1,12 @@
+let tUser: [string,number,boolean] ;
+
+tUser = ['hc',131,true] ; 
+//tUser.push(true) 
+// this is a problem in tuples it doesnt recognize these type of errors , that is you can use all types of push , etc array methods 
+
+type sampleType = [string,number,boolean];
+let sampleArray : sampleType = [
+    'dhatrish',
+    22,
+    true
+]
